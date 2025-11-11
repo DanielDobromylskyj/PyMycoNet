746d0e67bc454c26abdc2de33ef065e3f42293c9
DanielDobromylskyj <90842024+DanielDobromylskyj@users.noreply.github.com>
2025-11-11 11:15:55 +0000

    new buffer class

--- myconet/buffer.py
+++ myconet/buffer.py
@@ -1,189 +1,84 @@
-import pyopencl as pycl
-import numpy as np
+import numpy
+import pyopencl as cl
+
+mf = cl.mem_flags
 
 
-mf = pycl.mem_flags
+def multiply_array(shape):
+    total = 1
 
-def mul_array(shape):
-    if type(shape) is int:
-        return shape
+    for x in shape:
+        total *= x
 
-    total = shape[0]
-    for size in shape[1:]:
-        total *= size
     return total
 
-def create_network_buffer_from_input(cl, values):
-    """ A Standard way of converting input values into a network buffer object"""
-    np_array = np.array(values)
-    return NetworkBuffer(cl, np_array.ravel().astype(np.float32), np_array.shape)
+
+class ClInstance:
+    def __init__(self):
+        self.ctx = cl.create_some_context()
+        self.queue = cl.CommandQueue(self.ctx, device=None)
 
 
-def combine_buffers(cl, buffers):  # todo - find a better / faster way of doing this
-    """ Appends several buffers one after another to make a larger single buffer """
-    summed = np.array([], dtype=np.float32)
-    for buffer in buffers:
-        summed = np.append(summed, buffer.get_as_array())
-    return NetworkBuffer(cl, summed, summed.shape)
-
-
-def convert_gradients_to_buffer_list(cl, buffer, chunk_size):
-    """ Helper function to convert gradients to a buffer list object for easier usage"""
-    data = buffer.get_as_array()
-    chunks = [Gradients(cl, data[i:i + chunk_size]) for i in range(0, len(data), chunk_size)]
-    return BufferList(cl, chunks)
-
-
-def rearrange_feature_map_output(cl, outputs):
-    """ Converts a feature map output into a single bufferlist object"""
-    input_gradients = []
-    weight_gradients = []
-    bias_gradients = []
-
-    for kernel in outputs:
-        input_gradients.append(kernel[0])
-        weight_gradients.append(kernel[1])
-        bias_gradients.append(kernel[2])
-
-    return BufferList(cl, input_gradients), BufferList(cl, weight_gradients), BufferList(cl, bias_gradients)
-
-
-def create_empty_buffer(cl, shape, dtype=np.float32):
-    return EmptyNetworkBuffer(cl, shape, dtype)
 
 class EmptyNetworkBuffer:
-    def __init__(self, cl, shape, dtype, create_buffer=True):
-        self.cl = cl
+    def __init__(self, cl_instance: ClInstance, shape, item_dtype):
+        self.__cl = cl_instance
         self.__shape = shape
-        self.__dtype = dtype
+        self.__dtype = item_dtype
+        self.__item_count = multiply_array(shape)
+        self.__size = self.__item_count * item_dtype.itemsize
 
-        if create_buffer:
-            self.buffer = pycl.Buffer(self.cl.ctx, mf.READ_WRITE, size=mul_array(shape) * np.dtype(dtype).itemsize, hostbuf=None)
+        self.__cl_buffer = cl.Buffer(self.__cl.ctx, mf.READ_WRITE, size=self.__size, hostbuf=None)
+        self.__np_buffer = numpy.empty(self.__size, dtype=self.__dtype)
 
-    def write_to_buffer(self, array, offset=0):
-        pycl.enqueue_copy(self.cl.queue, self.buffer, array, device_offset=offset * np.dtype(self.__dtype).itemsize)
+        self.__last_sync = None
+        self.__sync_np_to_cl()
 
-    def force_shape(self, shape):
-        self.__shape = shape
 
-    def get_dtype(self):
+    @property
+    def cl(self):
+        if self.__last_sync != "cl":
+            self.__sync_np_to_cl()
+        return self.__cl_buffer
+
+    @property
+    def np(self):
+        if self.__last_sync != "np":
+            self.__sync_cl_to_np()
+        return self.__np_buffer
+
+
+    @property
+    def dtype(self):
         return self.__dtype
 
-    def get_shape(self):
-        """ Returns the shape of the buffer """
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def shape(self):
         return self.__shape
 
-    def is_image(self):
-        """ Returns True if the buffer contains an image (Or has more than 1 dimension)"""
-        return len(self.get_shape()) > 1
+    @property
+    def sync_location(self):
+        return self.__last_sync
 
-    def is_rgb_image(self):
-        """ Returns True if the buffer contains an RGB image (Or has more than 2 dimension)"""
-        return len(self.get_shape()) > 2
 
-    def get_as_buffer(self):
-        """ Returns the data as a GPU Buffer"""
-        return self.buffer
+    def __sync_np_to_cl(self):
+        cl.enqueue_copy(self.__cl.queue, self.__cl_buffer, self.__np_buffer).wait()
+        self.__last_sync = "cl"
 
-    def get_as_array(self):
-        """ Converts the buffer into a numpy array for CPU usage"""
-        output = np.empty(self.get_shape(), dtype=np.float32)
-        pycl.enqueue_copy(self.cl.queue, output, self.get_as_buffer()).wait()
-        return output
+    def __sync_cl_to_np(self):
+        cl.enqueue_copy(self.__cl.queue, self.__np_buffer, self.__cl_buffer).wait()
+        self.__last_sync = "np"
 
-    def release(self):
-        self.buffer.release()
 
-    def get_and_release(self):
-        array = self.get_as_array()
-        self.release()
-        return array
-
-    def __add__(self, other):
-        if isinstance(other, NetworkBuffer) or isinstance(other, Gradients):
-            return NetworkBuffer(self.cl, self.get_as_array() + other.get_as_array(), self.__shape)
-        return NetworkBuffer(self.cl, self.get_as_array() + other, self.__shape)
-
-    def __truediv__(self, other):
-        return NetworkBuffer(self.cl, self.get_as_array() / other, self.__shape)
-
-    def __len__(self):
-        return self.get_shape()[0]
-
+    def write_to_buffer(self, array: numpy.ndarray, offset=0):
+        cl.enqueue_copy(self.__cl.queue, self.np, array, device_offset=offset * numpy.dtype(self.__dtype).itemsize)
 
 
 class NetworkBuffer(EmptyNetworkBuffer):
-    def __init__(self, cl, data: np.ndarray, shape: tuple[int, ...]):
-        super().__init__(cl, shape, data.dtype, create_buffer=False)
-        self.buffer = pycl.Buffer(self.cl.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, data.nbytes, hostbuf=data)
-
-
-class BufferList:
-    def __init__(self, cl, buffers):
-        self.__cl = cl
-        self.__buffers = buffers
-
-    def get_as_buffer(self, index=None):
-        """ Return a single buffer at a given index. If a index is not given, return every buffer concatenated together (As a GPU Buffer)"""
-        if index is None:
-            return combine_buffers(self.__cl, self.__buffers).get_as_buffer()
-        else:
-            return self.__buffers[index].get_as_buffer()
-
-    def get_network_buffer(self, index):
-        """ Returns the raw buffer class at a given index"""
-        return self.__buffers[index]
-
-    def get_as_array(self, index=None):
-        """ Returns array form of buffer list (or index of if given)"""
-        if index is None:
-            return tuple([
-                buffer.get_as_array()
-                for buffer in self.__buffers
-            ])
-
-        else:
-            return self.__buffers[index].get_as_array()
-
-    def __len__(self):
-        return len(self.__buffers)
-
-    def __iter__(self):
-        for buffer in self.__buffers:
-            yield buffer
-
-    def __getitem__(self, item):
-        return self.__buffers[item]
-
-    def __setitem__(self, key, value):
-        self.__buffers[key] = value
-
-
-class Gradients:
-    def __init__(self, cl, gradients: np.ndarray):
-        self.__cl = cl
-        self.__gradients = pycl.Buffer(self.__cl.ctx, mf.READ_WRITE | mf.USE_HOST_PTR, hostbuf=gradients.astype(np.float32))
-        self.__shape = gradients.shape
-
-    def get_shape(self):
-        """ Returns the shape of the buffer"""
-        return self.__shape
-
-    def get_as_buffer(self):
-        """ Returns the data as a GPU Buffer"""
-        return self.__gradients
-
-    def get_as_array(self):
-        """ Returns the data as a CPU numpy array"""
-        output = np.empty(self.get_shape(), dtype=np.float32)
-        pycl.enqueue_copy(self.__cl.queue, output, self.get_as_buffer()).wait()
-        return output
-
-    def __add__(self, other_gradients):
-        return Gradients(self.__cl, self.get_as_array() + other_gradients.get_as_array())
-
-    def __truediv__(self, other):
-        return Gradients(self.__cl, self.get_as_array() / other)
-
-    def __neg__(self):
-        return Gradients(self.__cl, self.get_as_array() * -1)
+    def __init__(self, cl_instance: ClInstance, array: numpy.ndarray):
+        super().__init__(cl_instance, array.shape, array.dtype)
+        self.write_to_buffer(array, offset=0)
