Something wrong with backpropagation
Its to-do with the reducer

myconet/buffer.py
@@ -1,10 +1,14 @@
 import numpy
+import numpy as np
 import pyopencl as cl
 
 mf = cl.mem_flags
 
 
 def multiply_array(shape):
+    if type(shape) == int:
+        return shape
+
     total = 1
 
     for x in shape:
@@ -26,7 +30,7 @@ class EmptyNetworkBuffer:
         self.__shape = shape
         self.__dtype = item_dtype
         self.__item_count = multiply_array(shape)
-        self.__size = self.__item_count * item_dtype.itemsize
+        self.__size = self.__item_count * np.dtype(item_dtype).itemsize
 
         self.__cl_buffer = cl.Buffer(self.__cl.ctx, mf.READ_WRITE, size=self.__size, hostbuf=None)
         self.__np_buffer = numpy.empty(self.__item_count, dtype=self.__dtype)
@@ -65,11 +69,11 @@ class EmptyNetworkBuffer:
         return self.__last_sync
 
 
-    def __sync_np_to_cl(self):
+    def __sync_np_to_cl(self):  # todo - see about making a function that returns the enqueue event (for async stuff)
         cl.enqueue_copy(self.__cl.queue, self.__cl_buffer, self.__np_buffer).wait()
         self.__last_sync = "cl"
 
-    def __sync_cl_to_np(self):
+    def __sync_cl_to_np(self):  # todo - see about making a function that returns the enqueue event (for async stuff)
         cl.enqueue_copy(self.__cl.queue, self.__np_buffer, self.__cl_buffer).wait()
         self.__last_sync = "np"
 

myconet/layers/populated.py
@@ -1,6 +1,9 @@
+import numpy as np
+
+from CNN_Python_Opencl.myconet.buffer import EmptyNetworkBuffer
 from .default import Layer as DefaultLayer
 from .util import weight_init, bias_init
-from ..buffer import NetworkBuffer
+from ..buffer import NetworkBuffer, EmptyNetworkBuffer
 
 
 """
@@ -67,6 +70,72 @@ class FullyPopulated(DefaultLayer):
         layer = FullyPopulated(values['input_size'], values['output_size'], values['activation'], loading=True)
         layer.__weights = NetworkBuffer(cl_instance, values['weights'])
         layer.__biases  = NetworkBuffer(cl_instance, values['biases'])
-
         return layer
+
+    def forward(self, inputs: NetworkBuffer, batch: int=1) -> EmptyNetworkBuffer | NetworkBuffer:
+        unreduced_outputs = EmptyNetworkBuffer(self.cl, self.weight_count * batch, np.float32)
+        outputs = EmptyNetworkBuffer(self.cl, self.output_node_count * batch, np.float32)
+
+        stage_1 = self.forward_kernel.forward(
+            self.cl.queue, (batch, self.input_node_count, self.output_node_count), None,
+            inputs.cl,
+            unreduced_outputs.cl,
+            self.__weights.cl,
+            np.int32(self.input_node_count),
+            np.int32(self.output_node_count)
+        )
+
+        self.forward_kernel.reducer(
+            self.cl.queue, (batch, self.output_node_count), None,
+            unreduced_outputs.cl,
+            outputs.cl,
+            self.__biases.cl,
+            np.int32(self.__activation),
+            np.int32(self.input_node_count),
+            np.int32(self.output_node_count),
+            wait_for=[stage_1]
+        ).wait()
+
+        return outputs
+
+
+    def backward(self, inputs: NetworkBuffer, outputs: NetworkBuffer, previous_error: NetworkBuffer, learning_rate: float, batch: int=1):
+        unreduced_next_error_gradients = EmptyNetworkBuffer(self.cl, self.weight_count * batch, np.float32)
+        reduced_next_error_gradients = EmptyNetworkBuffer(self.cl, self.input_node_count * batch, np.float32)
+
+        weight_gradients = EmptyNetworkBuffer(self.cl, self.weight_count * batch, np.float32)
+        bias_gradients = EmptyNetworkBuffer(self.cl, self.bias_count * batch, np.float32)
+
+        stage_1 = self.backward_kernel.backward(
+            self.cl.queue, (batch, self.input_node_count, self.output_node_count), None,
+            inputs.cl,
+            outputs.cl,
+            self.__weights.cl,
+
+            previous_error.cl,
+            unreduced_next_error_gradients.cl,
+
+            weight_gradients.cl,
+            bias_gradients.cl,
+
+            np.int32(self.input_node_count),
+            np.int32(self.output_node_count),
+
+            np.int32(self.__activation),
+            np.float32(learning_rate)
+        )
+
+        self.backward_kernel.reducer(
+            self.cl.queue, (batch, self.input_node_count), None,
+            unreduced_next_error_gradients.cl,
+            reduced_next_error_gradients.cl,
+
+            np.int32(self.input_node_count),
+            np.int32(self.output_node_count),
+
+            wait_for=[stage_1]
+        ).wait()
+
+        return reduced_next_error_gradients, weight_gradients, bias_gradients
+
 

myconet/network.py
@@ -1,8 +1,10 @@
 import time
 
+import numpy as np
+
 from . import file_api
 from .logger import Logger
-from .buffer import ClInstance
+from .buffer import ClInstance, NetworkBuffer, EmptyNetworkBuffer
 from .file_api import decode_dict
 from .layers.lookup import lookup_table as layer_lookup_table
 
@@ -66,6 +68,95 @@ class Network:
 
         for layer in self.__layout:
             layer.load_values()
+
+    @staticmethod
+    def unflatten_samples(flat, shapes):
+        samples = []
+        idx = 0
+        for shape in shapes:
+            size = np.prod(shape)
+            chunk = flat[idx: idx + size]
+            samples.append(chunk.reshape(shape))
+            idx += size
+        return samples
+
+    @property
+    def output_shape(self):
+        return self.__layout[-1].output_node_count
+
+    def forward(self, inputs: list | tuple | np.ndarray, is_batch=False, get_all_data=False):
+        batch = len(inputs) if is_batch is True else 1
+
+        if type(inputs) is not np.ndarray:  # todo - ensure even if they pass a ndarray, its float32
+            inputs = np.array(inputs, dtype=np.float32)
+
+        if is_batch:
+            self.__log.log(f"Processing batch of {len(inputs)} inputs...")
+            inputs = np.concatenate([s.ravel() for s in inputs])
+
+        next_inputs = NetworkBuffer(self.cl, inputs)
+        data = [next_inputs] if get_all_data else []
+
+        for i, layer in enumerate(self.__layout):
+            next_inputs = layer.forward(next_inputs, batch=batch)
+
+            if get_all_data:
+                data.append(next_inputs)
+
+        if is_batch:
+            final_values = self.unflatten_samples(next_inputs.np, [self.output_shape for _ in range(batch)])
+
+            if get_all_data:
+                return final_values, data
+            return final_values
+
+        if get_all_data:
+            return next_inputs.np,  data
+        return next_inputs.np
+
+    def backward(self, inputs: list | tuple | np.ndarray,
+                 targets: list | tuple | np.ndarray,
+                 learning_rate, is_batch=False) -> list[list[None | np.ndarray]]:
+        """
+        Returns the gradients calculated from backpropagation in the form of np arrays,
+        Formated so they can be indexed by: [layer_index][0/1 for weights/biases][batch_index]
+
+        :param inputs:
+        :param targets:
+        :param learning_rate:
+        :param is_batch:
+        :return:
+        """
+        batch = len(inputs) if is_batch is True else 1
+
+        if type(targets) is not np.ndarray:  # todo - Ensure even if they pass a ndarray, its float32
+            targets = np.array(targets, dtype=np.float32)
+
+        if is_batch:
+            targets = np.concatenate([s.ravel() for s in targets])
+
+        outputs, all_layer_values = self.forward(inputs, is_batch, get_all_data=True)
+
+        # todo - Ensure this is the correct way around (targets - outputs) vs (outputs - targets)
+        previous_errors = NetworkBuffer(self.cl, (targets - outputs).astype(np.float32))
+
+        gradients = [[None, None] for _ in self.__layout]
+        for layer_index in range(len(self.__layout)-1, -1, -1):
+            layer = self.__layout[layer_index]
+
+            layer_inputs = all_layer_values[layer_index]
+            layer_outputs = all_layer_values[layer_index+1]
+
+            previous_errors, weight_gradients, bias_gradients = layer.backward(
+                layer_inputs, layer_outputs, previous_errors, learning_rate, batch
+            )
+
+            batched_weights = self.unflatten_samples(weight_gradients.np, [layer.weight_count for _ in range(batch)])
+            batched_biases = self.unflatten_samples(bias_gradients.np, [layer.bias_count for _ in range(batch)])
+
+            gradients[layer_index] = [batched_weights, batched_biases]
+
+        return gradients
 
     @staticmethod
     def __get_optimiser_id():  # todo